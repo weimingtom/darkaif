4080db8eb988f7a570393510b649a06100b8cfd3
darkneter <darkneter@4b42d02d-b03b-0410-94d2-ed925accbbea>
2009-02-16 00:46:26 +0000

    updating some server.

--- mmo/mmoj/src/mmo/Game.java
+++ mmo/mmoj/src/mmo/Game.java
@@ -2,6 +2,8 @@ package mmo;
 
 //import MMOJAServer;
 
+//import MMOJAServer;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -13,34 +15,68 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game {
-	public Player[] user;//list of user connect to the server
+public class Game extends Thread{
+	public boolean m_bRunThread = true;
+	public static Player[] user= new Player[64];//list of user connect to the server
 	static protected List<PrintWriter> writers = new ArrayList<PrintWriter>();
-	
+	public static List<Player> list = new ArrayList<Player>();
+	 //ArrayList<?> al = new ArrayList();
+	public double time = 0;
+	public double maxtime = 1000000;
 	//Player currentPlayer;
 	public Game(){
-		System.out.println("Game Class Running");
-		//while(true){
-		//	System.out.println("....");
-		//}
+		System.out.println("Game Class Running...");
+		/*
+		while(true){
+			for (int iplayer=0; iplayer<list.size();iplayer++) {
+				System.out.print("++++ id:>"+list.get(iplayer).m_clientID+"\n");
+			}
+		}
+		*/
 	}
-	
-	public class Player extends Thread {
-		double x = 0;
-		double y = 0;
-		double z = 0;
-		String objecttype = "box";
-		boolean forward = false;
-		boolean backward = false;
-		boolean right = false;
-		boolean left = false;
+	public void run() {
+		while(m_bRunThread){
+			time++;
+			if (time > maxtime ){
+				time = 0;
+				for (int iplayer=0; iplayer<list.size();iplayer++) {
+					System.out.print("++++ id:>"+list.get(iplayer).m_clientID+"x:"+list.get(iplayer).x+"y:"+list.get(iplayer).y +"\n");
+					if (list.get(iplayer).forward == true){
+					list.get(iplayer).y--;
+					}
+					if (list.get(iplayer).backward == true){
+						list.get(iplayer).y++;
+					}
+					if (list.get(iplayer).right == true){
+						list.get(iplayer).x++;
+					}
+					if (list.get(iplayer).left == true){
+						list.get(iplayer).x--;
+					}
+					String senddata = list.get(iplayer).objecttype + "{id:"+ list.get(iplayer).m_clientID +",x:" + list.get(iplayer).x + ",y:"+list.get(iplayer).y+"}"+"\n";
+					list.get(iplayer).sendallclient(senddata);
+				}
+			}
+		}
+	}
 
-		Socket socket;
-        BufferedReader input;
-        PrintWriter output;
-        Socket m_clientSocket;  
-		int m_clientID = -1;
-		boolean m_bRunThread = true;
+	public class Player extends Thread {
+		public double x = 0;
+		public double y = 0;
+		public double z = 0;
+		public String objecttype = "box";
+		public boolean forward = false;
+		public boolean backward = false;
+		public boolean right = false;
+		public boolean left = false;
+
+		public Socket socket;
+		public BufferedReader input;
+		public PrintWriter output;
+		public Socket m_clientSocket;  
+		public int m_clientID = -1;
+		public boolean m_bRunThread = true;
+		public PrintWriter broad = null;
 		
 		public Player(Socket accept,int clientID) {
 			m_clientSocket = accept;
@@ -49,7 +85,7 @@ public class Game {
 
 		public void cmd(String clientCommand,PrintWriter out){
 			if (clientCommand.indexOf( "cmd" )>=0){
-            	System.out.print("CMD FOUND! \n");
+            	//System.out.print("CMD FOUND! \n");
             	String cmdstring = clientCommand;
             	String[] cmdstr; 
             	int openbracket = 0;
@@ -70,7 +106,7 @@ public class Game {
             	if (cmdmove.indexOf("forward") >= 0){
             		if (cmdbool.indexOf("true") >= 0 ){
             			forward = true;
-            			y--;
+            			//y--;
             			//System.out.print("[-forward--TRUE-]" +"\n");
             		}else if (cmdbool.indexOf("false") >= 0 ){
             			//y++;
@@ -82,7 +118,7 @@ public class Game {
             	if (cmdmove.indexOf("backward") >= 0){//||(cmdstr[1] == "true")){
             		if (cmdbool.indexOf("true") >= 0 ){
             			backward = true;
-            			y++;
+            			//y++;
             			//System.out.print("[-forward--TRUE-]" +"\n");
             		}else if (cmdbool.indexOf("false") >= 0 ){
             			//y++;
@@ -96,7 +132,7 @@ public class Game {
             	if (cmdmove.indexOf("left") >= 0){
             		if (cmdbool.indexOf("true") >= 0 ){
             			left = true;
-            			x--;
+            			//x--;
             			//System.out.print("[-forward--TRUE-]" +"\n");
             		}else if (cmdbool.indexOf("false") >= 0 ){
             			//y++;
@@ -108,7 +144,7 @@ public class Game {
             	if (cmdmove.indexOf("right") >= 0){//||(cmdstr[1] == "true")){
             		if (cmdbool.indexOf("true") >= 0 ){
             			right = true;
-            			x++;
+            			//x++;
             			//System.out.print("[-forward--TRUE-]" +"\n");
             		}else if (cmdbool.indexOf("false") >= 0 ){
             			//y++;
@@ -119,10 +155,26 @@ public class Game {
             	}
             	//=================================================================================
             	
-            	System.out.print(x + ":" + y +"\n");
+            	//System.out.print(x + ":" + y +"\n");
             	out.println(objecttype + "{id:"+ m_clientID +",x:" + x + ",y:"+y+"}"+"\n");
                 out.flush();
+                String senddata = objecttype + "{id:"+ m_clientID +",x:" + x + ",y:"+y+"}"+"\n";
+                sendallclient(senddata);
             }
+		}
+		
+		public void sendallclient(String clientCommand){
+			for (int i=0; i<Game.writers.size();i++) 
+            {
+               broad = Game.writers.get(i);
+               broad.println(clientCommand);
+               broad.flush();
+            }
+			
+			//System.out.print("player numbers:"+list.toArray().length+"\n");
+			for (int iplayer=0; iplayer<list.size();iplayer++) {
+				System.out.print("array client id:>"+list.get(iplayer).m_clientID+"\n");
+			}
 		}
 		
 		public void run() {
@@ -138,12 +190,29 @@ public class Game {
         		BufferedOutputStream os = new BufferedOutputStream(m_clientSocket.getOutputStream());
             	//out = new PrintWriter(m_clientSocket.getOutputStream(), true);
             	//in = new BufferedReader(new InputStreamReader(m_clientSocket.getInputStream()));
-                //MMOJAServer.writers.add(out);
+                Game.writers.add(out);
                 // At this point, we can read for input and reply with appropriate output.
                 
                 // Run in a loop until m_bRunThread is set to false
                 while(m_bRunThread)
                 {   
+                	/*
+                	if (forward == true){
+                		y--;
+                	}
+                	
+                	if (backward == true){
+                		y++;
+                	}
+                	
+                	if (right == true){
+                		x++;
+                	}
+                	
+                	if (left == true){
+                		x--;
+                	}
+                	*/
                 	String clientCommand = in.readLine();
                 	
                     // read incoming stream
@@ -157,8 +226,8 @@ public class Game {
                     else
                     {
                         // Echo it back to the client.
-                        out.println(clientCommand);
-                        out.flush();
+                        //out.println(clientCommand);
+                        //out.flush();
                         //System.out.print(clientCommand.indexOf( "cmd" ) + ":");
                         //System.out.print(clientCommand.indexOf( "forward" ) + ":");
                         //System.out.print(clientCommand.indexOf( "true" ) + ":");
@@ -182,6 +251,13 @@ public class Game {
                     out.close();
                     m_clientSocket.close();
                     System.out.println("...Stopped");
+                    //clean out the socket just currently on the list player disconnect to the server
+                    for (int iplayer=0; iplayer<list.size();iplayer++) {
+        				System.out.print("array:>"+list.get(iplayer).m_clientID+"\n");
+        				if (m_clientID == list.get(iplayer).m_clientID){
+        					list.remove(iplayer);
+        				}
+        			}
                 }
                 catch(IOException ioe)
                 {
