817cf58b9e1057a0a1a14cc928140589c8dca7f1
darkneter <darkneter@4b42d02d-b03b-0410-94d2-ed925accbbea>
2009-07-29 18:59:14 +0000

    update... building just a simple gui...

---
+++ trunk/projects/gearunitsclientserver/gearunits_server_gui.py
@@ -0,0 +1,89 @@
+"""
+GUI SERVER BUILD
+Python version: 3.1.0
+"""
+
+from tkinter import *
+from tkinter import ttk
+
+host = '127.0.0.1';
+port = 5555;
+totalclient = 0;
+serverstatus = 'Off';
+title = "Gear Units > Flash Game Server";
+
+class Application(Frame):
+	#label_serverstatus = Label()
+	def say_hi(self):
+		print("hi there, everyone!")
+		
+	def button_start(self):
+		serverstatus  = 'On';
+		self.label_serverstatus["text"] = serverstatus
+		print("starting server...");
+	def button_stop(self):
+		serverstatus  = 'Off';
+		print("stopping server...");
+		self.label_serverstatus["text"] = serverstatus
+
+	def createWidgets(self):
+		self.text_host = Label(self)
+		self.text_host["text"] = "Host IP:"
+		self.text_host.grid(column=1, row=1, sticky=W)
+		
+		self.text_serverip = Entry(self, width=14, textvariable=host)
+		self.text_serverip.focus()
+		self.text_serverip.insert(0,host)
+		self.text_serverip.grid(column=2, row=1, sticky=W)
+		
+		self.text_serverport = Label(self)
+		self.text_serverport["text"] = "Port:"
+		self.text_serverport.grid(column=3, row=1, sticky=W)
+		
+		self.text_serverportnum = Entry(self, width=5, textvariable=port)
+		self.text_serverportnum.insert(0,port)
+		self.text_serverportnum.grid(column=4, row=1, sticky=W)
+		
+		self.text_gamestats = Label(self)
+		self.text_gamestats["text"] = "Game Server"
+		self.text_gamestats.grid(column=1, row=2, sticky=W)
+		
+		self.text_numberclient = Label(self)
+		self.text_numberclient["text"] = "Number of Client:"
+		self.text_numberclient.grid(column=1, row=3, sticky=W)
+		
+		self.text_numclient = Label(self)
+		self.text_numclient["text"] = totalclient
+		self.text_numclient.grid(column=2, row=3, sticky=W)
+		
+		self.text_serverstatus = Label(self)
+		self.text_serverstatus["text"] = "Server Status:"
+		self.text_serverstatus.grid(column=1, row=4, sticky=W)
+		
+		self.label_serverstatus = Label(self)
+		self.label_serverstatus["text"] = serverstatus
+		self.label_serverstatus.grid(column=2, row=4, sticky=W)
+
+		self.button_startserver = Button(self)
+		self.button_startserver["text"] = "Start Server"
+		self.button_startserver["command"] = self.button_start
+		self.button_startserver.grid(column=1, row=5, sticky=W)
+		#self.button_startserver.pack({"side":"left"})
+		
+		self.button_stopserver = Button(self)
+		self.button_stopserver["text"] = "Stop Server"
+		self.button_stopserver["command"] = self.button_stop
+		self.button_stopserver.grid(column=2, row=5, sticky=W)
+		
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.master.title(title)
+		self.pack()
+		self.createWidgets()
+		self.master.geometry("300x100")
+		#print(dir(self))
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
