1e58856155b0d013c4261a5f23096f74dfb60d65
darkneter <darkneter@4b42d02d-b03b-0410-94d2-ed925accbbea>
2009-08-10 00:43:27 +0000

    update database gui a bit.

--- projects/gearunitsclientserver/database_gui.py
+++ projects/gearunitsclientserver/database_gui.py
@@ -136,12 +136,28 @@ class Application(Frame):
 		
 		self.button_createrow = Button(self.tablerowframe)
 		self.button_createrow["text"] = "Create Table Row"
-		self.button_createrow["command"] = self.commit_save
+		self.button_createrow["command"] = self.createtablerow
 		self.button_createrow.grid(column=1, row=2, sticky=W)
 		
 		#TABLE FRAME ROW EDIT
+		####
 		self.tablerowframe = ttk.Labelframe(self, text='Table Rows EDIT', width=256, height=480)
-		self.tablerowframe.grid(column=1, row=4, sticky=W)
+		self.tablerowframe.grid(column=1, row=4, sticky=W,columnspan=2)
+		
+		self.button_deleterowtable = Button(self.tablerowframe)
+		self.button_deleterowtable["text"] = "Delete Table Row"
+		self.button_deleterowtable["command"] = self.deletetablerow
+		self.button_deleterowtable.grid(column=1, row=1, sticky=W)
+		
+		self.button_updatetablerow = Button(self.tablerowframe)
+		self.button_updatetablerow["text"] = "Update Table Row"
+		self.button_updatetablerow["command"] = self.updatetablerow
+		self.button_updatetablerow.grid(column=2, row=1, sticky=W)
+		
+		self.button_inserttablerow = Button(self.tablerowframe)
+		self.button_inserttablerow["text"] = "Insert Table Row"
+		self.button_inserttablerow["command"] = self.inserttablerow
+		self.button_inserttablerow.grid(column=3, row=1, sticky=W)
 		
 	#SELECT TABLE FROM COMBOBOX
 	def table_click(self,event):
@@ -162,6 +178,7 @@ class Application(Frame):
 			#spreadsheet.append(t[1])
 			self.listbox.insert("end", str(t[0]))
 		#self.listbox["listvariable"] = spreadsheet
+		self.deletetablerowinput()
 		
 	#CLICK TABLE ROW SELECTED ID OR ROW NAME
 	def listselect_click(self,event):
@@ -190,6 +207,7 @@ class Application(Frame):
 				
 				for col in comumndata:
 					col = col.strip()
+					print(col)
 					m = re.match(r"(^(\w+))",col)
 					#print (m.group(0))#column name of the table current selected.
 					self.tablevartext.append(m.group(0))
@@ -210,34 +228,53 @@ class Application(Frame):
 	def createtablerowinput(self):
 		self.deletetablerowinput()
 		rowcount = 0
-		#self.button_destroy = Button(self.tablerowframe)
-		#self.button_destroy["text"] = "delete rows"
-		#self.button_destroy["command"] = self.deletetablerowinput
-		#self.button_destroy.grid(column=1, row=1, sticky=W)
-		
-		self.button_deleterowtable = Button(self.tablerowframe)
-		self.button_deleterowtable["text"] = "Delete Table Row"
-		self.button_deleterowtable["command"] = self.deletetablerow
-		self.button_deleterowtable.grid(column=1, row=1, sticky=W)
-		
-		self.button_updatetablerow = Button(self.tablerowframe)
-		self.button_updatetablerow["text"] = "Update Table Row"
-		self.button_updatetablerow["command"] = self.updatetablerow
-		self.button_updatetablerow.grid(column=2, row=1, sticky=W)
-		
-		self.button_inserttablerow = Button(self.tablerowframe)
-		self.button_inserttablerow["text"] = "Insert Table Row"
-		self.button_inserttablerow["command"] = self.inserttablerow
-		self.button_inserttablerow.grid(column=3, row=1, sticky=W)
 		
 		for columnname in self.tablevartext:
 			#print(columnname)
+			#entry
 			self.entry_tablevarrow = Entry(self.tablerowframe, width=30, textvariable=columnname)
 			self.entry_tablevarrow.delete(0,"end")
 			self.entry_tablevarrow.insert(0,str(self.tablevarinput[rowcount]))
 			self.entry_tablevarrow.grid(column=2, row=rowcount+2, sticky=W)
 			self.tableentrywidgetrow.append(self.entry_tablevarrow)
-			
+			#Text label
+			self.label_var = Label(self.tablerowframe, width=16)
+			self.label_var['text'] = columnname
+			self.label_var.grid(column=1, row=rowcount+2, sticky=W)
+			self.tablelabelwidgetrow.append(self.label_var)
+			#print(rowcount)
+			rowcount += 1
+	def createtablerow(self):
+		#print ("TABLE ROW ->")
+		self.tablevartext = []
+		
+		cur.execute("select * from sqlite_master WHERE type='table'");
+		for t in cur:
+			if self.tablename == t[1]:
+				#print (t)
+				tablestr = t[4].replace("CREATE TABLE " +self.tablename + " ",'')
+				tablestr = tablestr.replace("(","")
+				tablestr = tablestr.replace(")","")
+				comumndata = tablestr.split(',')
+				
+				for col in comumndata:
+					col = col.strip()
+					m = re.match(r"(^(\w+))",col)
+					#print (m.group(0))#column name of the table current selected.
+					self.tablevartext.append(m.group(0))
+				break
+		print ("NEW TABLE")
+		self.deletetablerowinput()
+		rowcount = 0
+		for columnname in self.tablevartext:
+			#print(columnname)
+			#entry
+			self.entry_tablevarrow = Entry(self.tablerowframe, width=30, textvariable=columnname)
+			self.entry_tablevarrow.delete(0,"end")
+			#self.entry_tablevarrow.insert(0,str(self.tablevarinput[rowcount]))
+			self.entry_tablevarrow.grid(column=2, row=rowcount+2, sticky=W)
+			self.tableentrywidgetrow.append(self.entry_tablevarrow)
+			#Text label
 			self.label_var = Label(self.tablerowframe, width=16)
 			self.label_var['text'] = columnname
 			self.label_var.grid(column=1, row=rowcount+2, sticky=W)
@@ -245,72 +282,85 @@ class Application(Frame):
 			#print(rowcount)
 			rowcount += 1
 	def updatetablerow(self):
-		count = 0;
-		#mysql_query("UPDATE  $db_table SET imageid='{$hashname}' WHERE idhash='{$IDIMAGE}'")
-		query = "UPDATE " + self.tablename
-		set = " SET "
-		where = "WHERE"
-		for x in self.tableentrywidgetrow:
-			if count == 0:
-				where = where + " " + x["textvariable"] + "=\'" + x.get() + "\' "
-			else:
-				if count == len(self.tableentrywidgetrow)-1:
-					set = set + "" + x["textvariable"] + "=\'" + x.get() + "\' "
-				else:
-					set = set + "" + x["textvariable"] + "=\'" + x.get() + "\', "
-				
-			print ( x["textvariable"] + "->" +  x.get())
-			count += 1
-		print (query + set + where)
-		querystring = query + set + where
-		self.applydata(querystring)
-		#print (len(self.tableentrywidgetrow))
-	def deletetablerow(self):
-		count = 0;
-		query = "DELETE FROM " + self.tablename + " WHERE "
-		delete = ""
-		for x in self.tableentrywidgetrow:
-			if count == len(self.tableentrywidgetrow)-1:
-				delete = delete + "" + x["textvariable"] + "=\'" + x.get() + "\' "
-			else:
-				delete = delete + "" + x["textvariable"] + "=\'" + x.get() + "\' AND "
-			count += 1
-		print (query + delete)
-		querystring = query + delete
-		self.applydata(querystring)
-		#print (len(self.tableentrywidgetrow))
-	def inserttablerow(self):
-		#cur.execute('INSERT INTO member (user, password) VALUES( "admin", "admin")')
-		count = 0;
-		query = "INSERT INTO " + self.tablename + "("
-		values = ""
-		insert = ""
-		#be sure it not the primary key else it will give error
-		for x in self.tableentrywidgetrow:
-			if count == len(self.tableentrywidgetrow)-1:
-				values = values + x["textvariable"] + ''
-				insert = insert + '\"' + x.get() + '\"'
-			else:
+		if len(self.tableentrywidgetrow) > 0:
+			count = 0;
+			#mysql_query("UPDATE  $db_table SET imageid='{$hashname}' WHERE idhash='{$IDIMAGE}'")
+			query = "UPDATE " + self.tablename
+			set = " SET "
+			where = "WHERE"
+			for x in self.tableentrywidgetrow:
 				if count == 0:
-					print ("PRIMARY KEY")
+					where = where + " " + x["textvariable"] + "=\'" + x.get() + "\' "
 				else:
-					values = values + x["textvariable"] + ','
-					insert = insert + '\"' + x.get() + '\",'
+					if count == len(self.tableentrywidgetrow)-1:
+						set = set + "" + x["textvariable"] + "=\'" + x.get() + "\' "
+					else:
+						set = set + "" + x["textvariable"] + "=\'" + x.get() + "\', "
 				
-			count += 1
+				print ( x["textvariable"] + "->" +  x.get())
+				count += 1
+			print (query + set + where)
+			querystring = query + set + where
+			self.applydata(querystring)
+			#print (len(self.tableentrywidgetrow))
+		else:
+			print ("NO TABLE ROW")
+	def deletetablerow(self):
+		if len(self.tableentrywidgetrow) > 0:
+			count = 0;
+			query = "DELETE FROM " + self.tablename + " WHERE "
+			delete = ""
+			for x in self.tableentrywidgetrow:
+				if count == len(self.tableentrywidgetrow)-1:
+					delete = delete + "" + x["textvariable"] + "=\'" + x.get() + "\' "
+				else:
+					delete = delete + "" + x["textvariable"] + "=\'" + x.get() + "\' AND "
+				count += 1
+			print (query + delete)
+			querystring = query + delete
+			self.applydata(querystring)
+			#print (len(self.tableentrywidgetrow))
+		else:
+			print ("NO TABLE ROW")
+	def inserttablerow(self):
+		if len(self.tableentrywidgetrow) > 0:
+		#cur.execute('INSERT INTO member (user, password) VALUES( "admin", "admin")')
+			count = 0;
+			query = "INSERT INTO " + self.tablename + "("
+			values = ""
+			insert = ""
+			#be sure it not the primary key else it will give error
+			for x in self.tableentrywidgetrow:
+				if count == len(self.tableentrywidgetrow)-1:
+					values = values + x["textvariable"] + ''
+					insert = insert + '\"' + x.get() + '\"'
+				else:
+					if count == 0:
+						print ("PRIMARY KEY")
+					else:
+						values = values + x["textvariable"] + ','
+						insert = insert + '\"' + x.get() + '\",'
+				
+				count += 1
 		
-		querystring = query + values + ") VALUES(" + insert + ")"
-		print (querystring)
-		self.applydata(querystring)
-		#print (len(self.tableentrywidgetrow))
+			querystring = query + values + ") VALUES(" + insert + ")"
+			print (querystring)
+			self.applydata(querystring)
+			#print (len(self.tableentrywidgetrow))
+		else:
+			print ("NO TABLE ROW")
 	
 	def deletetablerowinput(self):
 		for x in self.tableentrywidgetrow:
 			x.destroy()
 		self.tableentrywidgetrow = []
 		for y in self.tablelabelwidgetrow:
-			x.destroy()
+			y.destroy()
 		self.tablelabelwidgetrow = []
+		#if self.button_deleterowtable != None:
+			#self.button_deleterowtable.destroy()
+			#self.button_updatetablerow.destroy()
+			#self.button_inserttablerow.destroy()
 	
 	#SCRIPTS ++++++
 	def executescript(self):
